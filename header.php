dc8e975e4c48443bdb74af2aadf3d1e0dab2fcb3
Eric Defore <eric.defore@gmail.com>
2017-06-16 15:12:17 -0400

    Vertical align Social/Phone in Header

--- header.php
+++ header.php
@@ -54,70 +54,82 @@ if ( ! defined( 'ABSPATH' ) ) {
 
                 <header id="site-header">
 					
-					<div class="top-bar extra">
+					<div class="top-bar extra" data-equalizer data-equalize-on="medium">
 						
-						<div class="top-bar-left logo">
+						<div class="top-bar-left logo" data-equalizer-watch>
 							
-							<a href="<?php bloginfo( 'url' ); ?>" title="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
-                            <?php 
-                                echo wp_get_attachment_image( get_theme_mod( 'gscr_logo_image', 1 ), 'medium', false, array(
-                                    'title' => get_bloginfo( 'name' ) . ' - ' . get_bloginfo( 'description' ),
-                                    'alt' => get_bloginfo( 'name' ) . ' - ' . get_bloginfo( 'description' ),
-                                ) ); 
-                            ?>
-                           </a>
+							<div class="vertical-align">
+							
+								<a href="<?php bloginfo( 'url' ); ?>" title="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
+								<?php 
+									echo wp_get_attachment_image( get_theme_mod( 'gscr_logo_image', 1 ), 'medium', false, array(
+										'title' => get_bloginfo( 'name' ) . ' - ' . get_bloginfo( 'description' ),
+										'alt' => get_bloginfo( 'name' ) . ' - ' . get_bloginfo( 'description' ),
+									) ); 
+								?>
+							   </a>
+								
+							</div>
 							
 						</div>
 						
-						<div class="top-bar-right social">
+						<div class="top-bar-right social" data-equalizer-watch>
 							
-							<?php 
-								$social_accounts = array(
-									'gscr_facebook' => array( 
-										'label' => 'Facebook',
-										'icon' => 'facebook-square',
-									),
-									'gscr_twitter' => array( 
-										'label' => 'Twitter',
-										'icon' => 'twitter-square',
-									),
-									'gscr_pinterest' => array( 
-										'label' => 'Pinterest',
-										'icon' => 'pinterest-square',
-									),
-									'gscr_linkedin' => array( 
-										'label' => 'LinkedIn',
-										'icon' => 'linkedin-square',
-									),
-									'gscr_instagram' => array( 
-										'label' => 'Instagram',
-										'icon' => 'instagram',
-									),
-								);
+							<div class="vertical-align">
+							
+								<?php 
+									$social_accounts = array(
+										'gscr_facebook' => array( 
+											'label' => 'Facebook',
+											'icon' => 'facebook-square',
+										),
+										'gscr_twitter' => array( 
+											'label' => 'Twitter',
+											'icon' => 'twitter-square',
+										),
+										'gscr_pinterest' => array( 
+											'label' => 'Pinterest',
+											'icon' => 'pinterest-square',
+										),
+										'gscr_linkedin' => array( 
+											'label' => 'LinkedIn',
+											'icon' => 'linkedin-square',
+										),
+										'gscr_instagram' => array( 
+											'label' => 'Instagram',
+											'icon' => 'instagram',
+										),
+									);
 
-							foreach ( $social_accounts as $key => $social ) :
-								if ( get_theme_mod( $key, '' ) !== '' ) : ?>
+								foreach ( $social_accounts as $key => $social ) :
+									if ( get_theme_mod( $key, '' ) !== '' ) : ?>
 
-									<a class="social-icon" href="<?php echo get_theme_mod( $key, '' ); ?>" target="_blank" title="<?php echo sprintf( __( 'Connect with us on %s', 'good-shepherd-catholic-radio' ), $social['label'] ); ?>">
-										<span class="fa fa-2x fa-<?php echo $social['icon']; ?>"></span>
+										<a class="social-icon" href="<?php echo get_theme_mod( $key, '' ); ?>" target="_blank" title="<?php echo sprintf( __( 'Connect with us on %s', 'good-shepherd-catholic-radio' ), $social['label'] ); ?>">
+											<span class="fa fa-2x fa-<?php echo $social['icon']; ?>"></span>
+										</a>
+
+									<?php endif;
+								endforeach;
+
+								if ( get_theme_mod( 'gscr_rss_show', false ) === true ) : ?>
+
+									<a class="social-icon" href="<?php bloginfo( 'rss2_url' ); ?>" title="<?php _e( 'Get our RSS Feed', 'good-shepherd-catholic-radio' ); ?>">
+										<span class="fa fa-rss-square"></span>
 									</a>
 
-								<?php endif;
-							endforeach;
-
-							if ( get_theme_mod( 'gscr_rss_show', false ) === true ) : ?>
-
-								<a class="social-icon" href="<?php bloginfo( 'rss2_url' ); ?>" title="<?php _e( 'Get our RSS Feed', 'good-shepherd-catholic-radio' ); ?>">
-									<span class="fa fa-rss-square"></span>
-								</a>
-
-							<?php endif; ?>
+								<?php endif; ?>
+								
+							</div>
 						
 						</div>
 						
-						<div class="top-bar-right phone">
+						<div class="top-bar-right phone" data-equalizer-watch>
 							
-							<?php echo gscr_get_phone_number_link( get_theme_mod( 'gscr_phone_number', '1-517-513-3340' ), false, '', true ); ?>
+							<div class="vertical-align">
+							
+								<?php echo gscr_get_phone_number_link( get_theme_mod( 'gscr_phone_number', '1-517-513-3340' ), false, '', true ); ?>
+								
+							</div>
 							
 						</div>
 						
