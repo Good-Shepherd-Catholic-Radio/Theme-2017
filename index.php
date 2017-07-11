cbc823c2da2f00b2f654146c17adfe5ddd5b8bdc
Eric Defore <eric.defore@gmail.com>
2017-07-11 12:09:53 -0400

    Closes #34

--- index.php
+++ index.php
@@ -15,14 +15,36 @@ get_header();
 
 ?>
 
-<h1 class="page-title columns small-12">
-    <?php echo _x( 'Blog', 'Blog Header', 'good-shepherd-catholic-radio' ); ?>
-</h1>
+<div class="main-content">
+	
+	<div class="row">
+		
+		<div class="small-12 columns">
+
+			<h1 class="page-title">
+				
+				<?php if ( is_search() ) : ?>
+                	<?php printf( __( 'Search results for "%s"', 'good-shepherd-catholic-radio' ), get_search_query() ); ?>
+				<?php else : ?>
+					<?php echo _x( 'Blog', 'Blog Header', 'good-shepherd-catholic-radio' ); ?>
+				<?php endif; ?>
+				
+			</h1>
+			
+		</div>
+		
+	</div>
+
+	<?php
+
+	locate_template( '/includes/hooks/' . get_post_type() . '-hooks.php', true, true );
+
+	get_template_part( 'partials/loop/loop', get_post_type() ); 
+
+	?>
+	
+</div>
 
 <?php
-
-locate_template( '/includes/hooks/' . get_post_type() . '-hooks.php', true, true );
-
-get_template_part( 'partials/loop/loop', get_post_type() );
 
 get_footer();
