3deb000af4127235482e67025fcf10b3a0468599
Eric Defore <eric.defore@gmail.com>
2017-07-12 14:06:31 -0400

    Line up Header player with Headers on Home Page

--- header.php
+++ header.php
@@ -155,49 +155,57 @@ if ( ! defined( 'ABSPATH' ) ) {
 									<div class="jp-type-single">
 
 										<div class="jp-gui jp-interface row expanded">
+											
+											<div class="small-12 columns">
+												
+												<div class="row">
 
-											<div class="jp-controls small-2 medium-1 columns">
-												<button class="jp-play" role="button" tabindex="0">
-													<span class="fa fa-2x play-icon"></span>
-												</button>
-											</div>
-
-											<div class="title-container jp-details text-center medium-4 medium-pull-1 columns">
-												<div class="jp-title" aria-label="title">
-													<?php _e( 'Listen Live!', 'good-shepherd-catholic-radio' ); ?>
-												</div>
-											</div>
-
-											<div class="jp-volume-controls small-2 offset-small-1 medium-5 medium-pull-3 columns">
-
-												<div class="row expanded small-collapse">
-
-													<div class="small-2 small-offset-1 columns">
-
-														<button class="jp-mute" role="button" tabindex="0">
-															<span class="fa fa-2x mute-icon"></span>
+													<div class="jp-controls small-2 medium-1 columns">
+														<button class="jp-play" role="button" tabindex="0">
+															<span class="fa fa-2x play-icon"></span>
 														</button>
-
 													</div>
 
-													<div class="volume-bar-container small-5 columns">
+													<div class="title-container jp-details text-center medium-4 medium-pull-1 columns">
+														<div class="jp-title" aria-label="title">
+															<?php _e( 'Listen Live!', 'good-shepherd-catholic-radio' ); ?>
+														</div>
+													</div>
 
-														<div class="jp-volume-bar">
-															<div class="jp-volume-bar-value"></div>
+													<div class="jp-volume-controls small-2 offset-small-1 medium-5 medium-pull-3 columns">
+
+														<div class="row expanded small-collapse">
+
+															<div class="small-2 small-offset-1 columns">
+
+																<button class="jp-mute" role="button" tabindex="0">
+																	<span class="fa fa-2x mute-icon"></span>
+																</button>
+
+															</div>
+
+															<div class="volume-bar-container small-5 columns">
+
+																<div class="jp-volume-bar">
+																	<div class="jp-volume-bar-value"></div>
+																</div>
+
+															</div>
+
+															<div class="small-2 small-pull-1 columns">
+
+																<button class="jp-volume-max" role="button" tabindex="0">
+																	<span class="fa fa-2x volume-max-icon"></span>
+																</button>
+
+															</div>
+
 														</div>
 
 													</div>
-
-													<div class="small-2 small-pull-1 columns">
-
-														<button class="jp-volume-max" role="button" tabindex="0">
-															<span class="fa fa-2x volume-max-icon"></span>
-														</button>
-
-													</div>
-
+													
 												</div>
-
+												
 											</div>
 
 										</div>
