884bccdd6f5cff842b5842db651f5ce55466f32c
Eric Defore <eric.defore@gmail.com>
2017-07-25 08:24:32 -0400

    Footer Affiliates, Social Media Icons, and Copyright Information are all on a white background now. Closes #48

--- footer.php
+++ footer.php
@@ -37,67 +37,77 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	</div>
 	
-	<div class="row footer-affiliates">
-		
-		<?php dynamic_sidebar( 'footer-affiliates' ); ?>
+	<div class="footer-affiliates">
 	
-	</div>
-	
-	<div class="social row text-center">
+		<div class="row">
 
-		<div class="small-12 columns">
-
-			<?php 
-				$social_accounts = array(
-					'gscr_facebook' => array( 
-						'label' => 'Facebook',
-						'icon' => 'facebook-square',
-					),
-					'gscr_twitter' => array( 
-						'label' => 'Twitter',
-						'icon' => 'twitter-square',
-					),
-					'gscr_pinterest' => array( 
-						'label' => 'Pinterest',
-						'icon' => 'pinterest-square',
-					),
-					'gscr_linkedin' => array( 
-						'label' => 'LinkedIn',
-						'icon' => 'linkedin-square',
-					),
-					'gscr_instagram' => array( 
-						'label' => 'Instagram',
-						'icon' => 'instagram',
-					),
-				);
-			
-			foreach ( $social_accounts as $key => $social ) :
-				if ( get_theme_mod( $key, '' ) !== '' ) : ?>
-
-					<a class="social-icon" href="<?php echo get_theme_mod( $key, '' ); ?>" target="_blank" title="<?php echo sprintf( __( 'Connect with us on %s', 'good-shepherd-catholic-radio' ), $social['label'] ); ?>">
-						<span class="fa fa-<?php echo $social['icon']; ?>"></span>
-					</a>
-
-				<?php endif;
-			endforeach;
-			
-			if ( get_theme_mod( 'gscr_rss_show', false ) === true ) : ?>
-
-				<a class="social-icon" href="<?php bloginfo( 'rss2_url' ); ?>" title="<?php _e( 'Get our RSS Feed', 'good-shepherd-catholic-radio' ); ?>">
-					<span class="fa fa-rss-square"></span>
-				</a>
-
-			<?php endif; ?>
+			<?php dynamic_sidebar( 'footer-affiliates' ); ?>
 
 		</div>
+		
+	</div>
+	
+	<div class="social">
+	
+		<div class="row text-center">
 
+			<div class="small-12 columns">
+
+				<?php 
+					$social_accounts = array(
+						'gscr_facebook' => array( 
+							'label' => 'Facebook',
+							'icon' => 'facebook-square',
+						),
+						'gscr_twitter' => array( 
+							'label' => 'Twitter',
+							'icon' => 'twitter-square',
+						),
+						'gscr_pinterest' => array( 
+							'label' => 'Pinterest',
+							'icon' => 'pinterest-square',
+						),
+						'gscr_linkedin' => array( 
+							'label' => 'LinkedIn',
+							'icon' => 'linkedin-square',
+						),
+						'gscr_instagram' => array( 
+							'label' => 'Instagram',
+							'icon' => 'instagram',
+						),
+					);
+
+				foreach ( $social_accounts as $key => $social ) :
+					if ( get_theme_mod( $key, '' ) !== '' ) : ?>
+
+						<a class="social-icon" href="<?php echo get_theme_mod( $key, '' ); ?>" target="_blank" title="<?php echo sprintf( __( 'Connect with us on %s', 'good-shepherd-catholic-radio' ), $social['label'] ); ?>">
+							<span class="fa fa-<?php echo $social['icon']; ?>"></span>
+						</a>
+
+					<?php endif;
+				endforeach;
+
+				if ( get_theme_mod( 'gscr_rss_show', false ) === true ) : ?>
+
+					<a class="social-icon" href="<?php bloginfo( 'rss2_url' ); ?>" title="<?php _e( 'Get our RSS Feed', 'good-shepherd-catholic-radio' ); ?>">
+						<span class="fa fa-rss-square"></span>
+					</a>
+
+				<?php endif; ?>
+
+			</div>
+
+		</div>
+		
 	</div>
 
-	<div class="row">
-		<div class="small-12 columns text-center">
-			<?php echo sprintf( _x( 'Site by %s', 'Site By Footer Text', 'good-shepherd-catholic-radio' ), '<a href="//realbigmarketing.com/" target="_blank">Real Big Marketing</a>' ); ?>
-			<br />
-			<?php echo sprintf( _x( 'Copyright &copy; %s %s', 'Copyright Footer Text', 'good-shepherd-catholic-radio' ), date( 'Y' ), get_bloginfo( 'name' ) ); ?>
+	<div class="copyright">
+		<div class="row">
+			<div class="small-12 columns text-center">
+				<?php echo sprintf( _x( 'Site by %s', 'Site By Footer Text', 'good-shepherd-catholic-radio' ), '<a href="//realbigmarketing.com/" target="_blank">Real Big Marketing</a>' ); ?>
+				<br />
+				<?php echo sprintf( _x( 'Copyright &copy; %s %s', 'Copyright Footer Text', 'good-shepherd-catholic-radio' ), date( 'Y' ), get_bloginfo( 'name' ) ); ?>
+			</div>
 		</div>
 	</div>
 
