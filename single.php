be067272b1eca7a3205fabdf26c219e3538e720f
Eric Defore <eric.defore@gmail.com>
2017-08-31 13:03:03 -0400

    Header overlay for Posts. Closes #69

--- single.php
+++ single.php
@@ -17,19 +17,44 @@ locate_template( '/includes/hooks/' . get_post_type() . '-hooks.php', true, true
 get_header();
 
 the_post();
+
+global $has_featured_image;
+
 ?>
+
+<?php if ( $has_featured_image && get_post_type() !== 'tribe_events' ) : ?>
+
+	<div class="post-title">
+		<div class="post-title-color-overlay"></div>
+		<div class="post-title-text">
+			<h1>
+				<?php the_title(); ?>
+			</h1>
+			<?php if ( get_post_type() == 'post' ) : ?>
+				<span class="timestamp"><span class="fa fa-clock-o"></span>&nbsp;<?php the_date(); ?></span>
+			<?php endif; ?>
+		</div>
+	</div>
+
+<?php endif; ?>
 
 <div class="main-content">
 
 	<div class="row">
 
 		<article id="post-<?php the_ID(); ?>" <?php post_class( array( 'columns', 'small-12' ) ); ?>>
+			
+			<?php if ( ! $has_featured_image && 
+					  get_post_type() !== 'tribe_events' ) : ?>
 
-			<h1 class="post-title">
-				<?php the_title(); ?>
-			</h1>
+				<h1 class="post-title">
+					<?php the_title(); ?>
+				</h1>
+			
+			<?php endif; ?>
 
-			<?php if ( get_post_type() == 'post' ) : ?>
+			<?php if ( ! $has_featured_image && 
+					  get_post_type() == 'post' ) : ?>
 
 				<p>
 					<span class="timestamp"><span class="fa fa-clock-o"></span>&nbsp;<?php the_date(); ?></span>
