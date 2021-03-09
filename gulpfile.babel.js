b435d44449696abce70cf4f7d29ec4678806aa1e
Eric Defore <eric.defore@gmail.com>
2021-03-09 16:50:35 -0500

    Separate Source Map for CSS
    This should stop Page Speed testers from complaining about the CSS size.
    
    If not, we'll just remove source maps altogether (I would prefer we
    didn't as they're useful, but we could make it dev-only in that case)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -110,7 +110,7 @@ function sass() {
       $.if( PRODUCTION, $.cleanCss( { compatibility: 'ie9' } ) )
     )
     .pipe(
-      $.if( ! PRODUCTION, $.sourcemaps.write() )
+      $.if( ! PRODUCTION, $.sourcemaps.write( '.' ) )
     )
     .pipe(
       $.if( REVISIONING && PRODUCTION || REVISIONING && DEV, $.rev() )
