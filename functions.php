5427ed4cdbcbb60d418938f618ba89615c9086cf
Erin Leck <erinleck@gmail.com>
2021-03-11 09:37:25 -0700

    defer parsing of js

--- functions.php
+++ functions.php
@@ -619,3 +619,14 @@ add_action( 'init', function() {
 } );
 
 */
+
+/**
+ * Defers parsing of JS
+ * @since	{{ VERSION }}
+ */
+function defer_parsing_of_js ( $url ) {
+	if ( FALSE === strpos( $url, '.js' ) ) return $url;
+	if ( strpos( $url, 'jquery.js' ) ) return $url;
+	return "$url' defer ";
+	}
+	add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
